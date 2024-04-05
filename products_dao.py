59f0527cbf9008d589b19783ff5d0ac608e11219
FasinFasi <141424321+fasinfasi@users.noreply.github.com>
2024-04-05 23:24:05 +0530

    Update products_dao.py
    Format corrected

--- products_dao.py
+++ products_dao.py
@@ -1,12 +1,9 @@
 from sql_connection import get_sql_connection
 
 def get_all_products(connection):
-
     cursor = connection.cursor()
-
     query = ("SELECT products.product_id, products.product_name, products.uom_id, products.price_per_Unit, uom.uom_name "
             "FROM electronic_store.products INNER JOIN uom on products.uom_id=uom.uom_id")
-
     cursor.execute(query)
     
     response = []
@@ -24,14 +21,12 @@ def get_all_products(connection):
 
 def insert_new_product(connection, product):
     cursor = connection.cursor()
-    
     query = ("INSERT INTO products "
              "(product_name, uom_id, price_per_unit)"
              "VALUES (%s, %s, %s)")
     data = (product['product_name'], product['uom_id'], product['price_per_unit'])
     cursor.execute(query, data)
     connection.commit()
-    
     return cursor.lastrowid
 
 def delete_product(connection, product_id):
