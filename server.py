d147cb3568e04f324c248313b74d0f38fb049404
FasinFasi <141424321+fasinfasi@users.noreply.github.com>
2024-04-08 11:28:48 +0530

    delete function to server.py
    Add delete function to server as delete_product

--- server.py
+++ server.py
@@ -11,7 +11,16 @@ def get_products():
     response = jsonify(products)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
-    
+
+@app.route('/delete_product', methods=['POST'])
+def delete_product():
+    return_id = products_dao.delete_product(connection, request.form['product_id'])
+    response = jsonify({
+        'product_id': return_id
+    })
+    response.headers.app('Access-Control-Allow-Origin', '*')
+    return response
+
 if __name__=='__main__':
     print('Starting Python Flask server for Electronic Store Management System')
     app.run(port=5000)
