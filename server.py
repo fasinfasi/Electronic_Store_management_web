e87008083838fd2bb780a2a7b458481d8d5f6fed
FasinFasi <141424321+fasinfasi@users.noreply.github.com>
2024-04-19 17:33:54 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,6 @@
 from flask import Flask, request, jsonify
 from sql_connection import get_sql_connection
 import products_dao
-import uom_dao
-import order_dao
-import json
 
 app = Flask(__name__)
 connection = get_sql_connection()
@@ -15,41 +12,6 @@ def get_products():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
 
-@app.route('/insertProduct', methods=['POST'])
-def insert_product():
-    request_payload = json.loads(request.form['data'])
-    product_id = products_dao.insert_new_product(connection, request_payload)
-    response = jsonify({
-        'product_id': product_id
-        })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-@app.route('/getUOM', methods=['GET'])
-def get_uom():
-    response = uom_dao.get_uoms(connection)
-    response = jsonify(response)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-@app.route('/getAllOrders', methods=['GET'])
-def get_all_orders():
-    response = orders_dao.get_all_orders(connection)
-    response = jsonify(response)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-@app.route('/insertOrder', methods=['POST'])
-def insert_order():
-    request_payload = json.loads(request.form['data'])
-    order_id = order_dao.insert_order(connection, request_payload)
-    response = jsonify({
-        'order_id': order_id
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
-
-@app.route('/delete_product', methods=['POST'])
 def delete_product():
     return_id = products_dao.delete_product(connection, request.form['product_id'])
     response = jsonify({
